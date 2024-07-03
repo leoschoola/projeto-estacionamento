44ad1a571ce68c22c1ead5b817b1f0b394dc15e7
leoschoola <161724772+leoschoola@users.noreply.github.com>
2024-07-03 20:49:43 -0300

    Criando lista de clientes e diminuindo tamanho do código

--- projeto/Main.java
+++ projeto/Main.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
+
+        //Criando lista de clientes
+        ArrayList<TicketMensal> clientes = new ArrayList<>();
+
         //Declarando objetos
         Entrada entrada = new Entrada();
         Ticket ticket = new Ticket();
-        TicketMensal ticketMensal = new TicketMensal();
         Saida saida = new Saida();
 
         System.out.println("Bem vindo ao Estacionamento!");
@@ -18,14 +22,8 @@ public class Main {
         //Organizando respostas do admin
         int resposta;
         int cadastroClienteMensal;
-        String cpfCliente;
-        String veiculoClienteMensal;
-        String veiculoAvulso;
-        String cor;
-        String placa;
-        String modelo;
-        String marca;
 
+        //Iniciando código
         do {
             System.out.println("Selecione a opção: ");
             System.out.println("[1] TICKET AVULSO");
@@ -33,59 +31,61 @@ public class Main {
             System.out.println("[3] SAIR");
             resposta = tc.nextInt();
 
-            switch (resposta){
-                case 1:
+            switch (resposta){ 
+                case 1: //Se ticket for avulso
                     System.out.println("TICKET AVULSO");
                     System.out.println("");
                     System.out.println("TIPO DE VEÍCULO: ");
-                    veiculoAvulso = tc.next();
-                    ticket.setVeiculo(veiculoAvulso);
+                    System.out.println("CARRO OU MOTO? ");
+                    String veiculoCliente = tc.next();
+                    ticket.setVeiculo(veiculoCliente);
 
                     System.out.println("PLACA: ");
                     System.out.println("DEVE SER ESCRITO NESTE PADRAO");
                     System.out.println("XXXNNNN");
                     System.out.println("OU");
                     System.out.println("XXXNXNN");
-                    placa = tc.next();
+                    String placa = tc.next();
                     ticket.setPlaca(placa);
 
                     System.out.println("MARCA: ");
-                    marca = tc.next();
+                    String marca = tc.next();
                     ticket.setMarca(marca);
 
                     System.out.println("MODELO: ");
-                    modelo = tc.next();
+                    String modelo = tc.next();
                     ticket.setModelo(modelo);
 
-                    System.out.println("COR ");
-                    cor = tc.next();
+                    System.out.println("COR: ");
+                    String cor = tc.next();
                     ticket.setCor(cor);
 
                     System.out.println("\n");
                     System.out.println(ticket.toString());
                     break;
-                case 2:
+                case 2: //Se ticket for mensal
                     System.out.println("TICKET MENSAL");
                     System.out.println("");
-                    System.out.println("VOCÊ JÁ TEM CADASTRO?");
-                    System.out.println("[1] SIM");
-                    System.out.println("[2] NÃO");
+                    System.out.println("[1] CRIAR CADASTRO");
+                    System.out.println("[2] JÁ POSSUO CADASTRO");
                     cadastroClienteMensal = tc.nextInt();
                     if (cadastroClienteMensal == 1){
-                        System.out.println("Verificando seu cadastro...");
-                    } else {
                         tc.nextLine();
+
+                        TicketMensal novoCadastro = new TicketMensal();
+
                         System.out.println("Digite seu nome completo: ");
                         String nomeCliente = tc.nextLine();
-                        ticketMensal.setNome(nomeCliente);
+                        novoCadastro.setNome(nomeCliente);
 
                         System.out.println("Digite seu CPF: ");
-                        cpfCliente = tc.nextLine();
-                        ticketMensal.setCpf(cpfCliente);
+                        String cpfCliente = tc.nextLine();
+                        novoCadastro.setCpf(cpfCliente);
 
                         System.out.println("TIPO DE VEÍCULO: ");
-                        veiculoAvulso = tc.next();
-                        ticketMensal.setVeiculo(veiculoAvulso);
+                        System.out.println("CARRO OU MOTO? ");
+                        veiculoCliente = tc.next();
+                        novoCadastro.setVeiculo(veiculoCliente);
 
                         System.out.println("PLACA: ");
                         System.out.println("DEVE SER ESCRITO NESTE PADRAO");
@@ -93,25 +93,33 @@ public class Main {
                         System.out.println("OU");
                         System.out.println("XXXNXNN");
                         placa = tc.next();
-                        ticketMensal.setPlaca(placa);
+                        novoCadastro.setPlaca(placa);
 
                         System.out.println("MARCA: ");
                         marca = tc.next();
-                        ticketMensal.setMarca(marca);
+                        novoCadastro.setMarca(marca);
 
                         System.out.println("MODELO: ");
                         modelo = tc.next();
-                        ticketMensal.setModelo(modelo);
+                        novoCadastro.setModelo(modelo);
 
-                        System.out.println("COR ");
+                        System.out.println("COR: ");
                         cor = tc.next();
-                        ticketMensal.setCor(cor);
+                        novoCadastro.setCor(cor);
 
-                        System.out.println("\n");
-                        System.out.println(ticketMensal.toString());
+                        clientes.add(novoCadastro); //Adicionando os clientes a lista de clientes cadastrados
+
+                        System.out.println("\n Cadastro realizado com sucesso!");
+
+                    } else {
+                        System.out.println("Verificando seu cadastro...");
+                        System.out.println("\n Clientes Cadastrados: ");
+                        for (TicketMensal clientela : clientes){
+                            System.out.println(clientela.toString());
+                        }
                     }
                     break;
-                case 3:
+                case 3: //Registrando horário de saída
                     System.out.println("Horario de saída: " + saida.Horario());
                     System.out.println("Finalizando atendimento.");
                     break;
