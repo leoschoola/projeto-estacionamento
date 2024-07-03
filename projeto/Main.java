291d7e15976851ddc767534323f6bd16eb97aca4
leoschoola <161724772+leoschoola@users.noreply.github.com>
2024-07-02 23:04:44 -0300

    Alterando o nome "Estacionamento" para "Main" para organização

--- projeto/Main.java
+++ projeto/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Estacionamento {
+public class Main {
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
         //Declarando objetos
